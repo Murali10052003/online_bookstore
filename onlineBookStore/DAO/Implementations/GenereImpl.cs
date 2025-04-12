265fddab0fd8177cf7c6785b5e652ba1405c6699
Murali10052003 <131389194+Murali10052003@users.noreply.github.com>
2025-04-12 08:05:52 +0530

    Update GenereImpl.cs

--- onlineBookStore/DAO/Implementations/GenereImpl.cs
+++ onlineBookStore/DAO/Implementations/GenereImpl.cs
@@ -1,21 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using OnlineBookstore.Entity;
-using OnlineBookstore.Util;
-
 public class GenreDAOImpl : IGenreDAO
 {
+    private readonly DbConnectionUtil _dbUtil;
+
+    public GenreDAOImpl(DbConnectionUtil dbUtil)
+    {
+        _dbUtil = dbUtil;
+    }
+
     public List<Genre> GetAllGenres()
     {
         List<Genre> genres = new List<Genre>();
-
-        using (SqlConnection conn = DBConnectionUtil.GetConnection())
+        using (SqlConnection conn = _dbUtil.GetOpenConnection())
         {
             string query = "SELECT * FROM Genres";
             SqlCommand cmd = new SqlCommand(query, conn);
             SqlDataReader reader = cmd.ExecuteReader();
-
             while (reader.Read())
             {
                 genres.Add(new Genre
@@ -25,19 +24,17 @@ public class GenreDAOImpl : IGenreDAO
                 });
             }
         }
-
         return genres;
     }
 
     public Genre GetGenreById(int genreId)
     {
-        using (SqlConnection conn = DBConnectionUtil.GetConnection())
+        using (SqlConnection conn = _dbUtil.GetOpenConnection())
         {
             string query = "SELECT * FROM Genres WHERE GenreID = @GenreID";
             SqlCommand cmd = new SqlCommand(query, conn);
             cmd.Parameters.AddWithValue("@GenreID", genreId);
             SqlDataReader reader = cmd.ExecuteReader();
-
             if (reader.Read())
             {
                 return new Genre
@@ -47,7 +44,6 @@ public class GenreDAOImpl : IGenreDAO
                 };
             }
         }
-
         return null;
     }
 }
